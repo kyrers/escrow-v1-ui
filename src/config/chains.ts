9701a15ddf8d896258645465f72cfbca0e8159cb
kyrers <kyrers.eth@gmail.com>
2025-05-22 17:16:51 +0100

    Improved chain configuration +  add custom transports

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,7 +1,7 @@
 import { mainnet, sepolia, type AppKitNetwork } from "@reown/appkit/networks";
+import { http, type Transport } from "viem";
 
-export const ALL_CHAINS = [mainnet, sepolia] as const;
-
+//Use mainnet in production, otherwise use all chains
 export const SUPPORTED_CHAINS: Record<number, AppKitNetwork> = import.meta.env
   .PROD
   ? {
@@ -13,3 +13,12 @@ export const SUPPORTED_CHAINS: Record<number, AppKitNetwork> = import.meta.env
     };
 
 export const DEFAULT_CHAIN = import.meta.env.PROD ? mainnet.id : sepolia.id;
+
+export const TRANSPORTS: Record<number, Transport> = import.meta.env.PROD
+  ? {
+      [mainnet.id]: http(import.meta.env.VITE_ETHEREUM_MAINNET_RPC),
+    }
+  : {
+      [mainnet.id]: http(import.meta.env.VITE_ETHEREUM_MAINNET_RPC),
+      [sepolia.id]: http(import.meta.env.VITE_ETHEREUM_SEPOLIA_RPC),
+    };
--- src/config/reown.ts
+++ src/config/reown.ts
@@ -1,24 +1,23 @@
 import { createAppKit } from "@reown/appkit/react";
 import { type AppKitNetwork } from "@reown/appkit/networks";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { ALL_CHAINS, DEFAULT_CHAIN, SUPPORTED_CHAINS } from "./chains";
-import { mainnet } from "@reown/appkit/networks";
+import { DEFAULT_CHAIN, SUPPORTED_CHAINS, TRANSPORTS } from "./chains";
 
 if (!import.meta.env.VITE_REOWN_PROJECT_ID) {
   throw new Error("No VITE_REOWN_PROJECT_ID environment variable");
 }
 const projectId = import.meta.env.VITE_REOWN_PROJECT_ID;
 
-//The "as ..." is to fix a typescript error
-//Use mainnet in production, otherwise use all chains
-const networks = import.meta.env.PROD
-  ? ([mainnet] as [AppKitNetwork, ...AppKitNetwork[]])
-  : ([...ALL_CHAINS] as [AppKitNetwork, ...AppKitNetwork[]]);
+const networks = Object.values(SUPPORTED_CHAINS) as [
+  AppKitNetwork,
+  ...AppKitNetwork[]
+];
 
 export const wagmiAdapter = new WagmiAdapter({
-  networks,
+  networks: networks,
   projectId,
   ssr: true,
+  transports: TRANSPORTS,
 });
 
 createAppKit({
