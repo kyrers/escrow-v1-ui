6a0aa99676130244b02bbe7113c583dd9f328100
kyrers <kyrers.eth@gmail.com>
2025-08-25 19:08:26 +0100

    feat: customize alert to match styling but also include link to documentation

--- src/components/Warnings/SmartContractWalletWarning.tsx
+++ src/components/Warnings/SmartContractWalletWarning.tsx
@@ -1,4 +1,3 @@
-import { AlertMessage } from "@kleros/ui-components-library";
 import { IconButton } from "components/Common/Buttons/IconButton";
 import { useLocalStorage } from "hooks/useLocalStorage";
 import { useEffect, useState } from "react";
@@ -6,13 +5,53 @@ import styled from "styled-components";
 import { getCode } from "viem/actions";
 import { useAccount, useClient } from "wagmi";
 import CloseIcon from "assets/close.svg?react";
+import WarningCircleOutline from "assets/warning-circle-outline.svg?react";
 
 const Container = styled.div`
   display: flex;
 `;
 
-const StyledAlertMessage = styled(AlertMessage)`
+//This mimics the AlertMessage component from the Kleros UI Components Library.
+//If that component is updated to allow for the warning message to be an element instead of a string, we can simplify this component by just using <AlertMessage />
+const CustomAlert = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
   flex: 1;
+  padding: 16px;
+  background-color: ${({ theme }) => theme.colors.whiteBackground};
+  border: 1px solid ${({ theme }) => theme.colors.warning};
+  border-radius: ${({ theme }) => theme.radius.base};
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
+
+const StyledWarningCircleOutline = styled(WarningCircleOutline)`
+  path {
+    fill: ${({ theme }) => theme.colors.warning};
+  }
+`;
+
+const AlertContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledH2 = styled.h2`
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.warning};
+`;
+
+const StyledSmall = styled.small`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
+
+const StyledA = styled.a`
+  text-decoration: underline;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 const StyledIconButton = styled(IconButton)`
@@ -46,11 +85,23 @@ export default function SmartContractWalletWarning() {
 
   return (
     <Container>
-      <StyledAlertMessage
-        title="Warning"
-        msg="You are using a smart contract wallet. This is not recommended."
-        variant="warning"
-      />
+      <CustomAlert>
+        <StyledWarningCircleOutline width={24} height={24} />
+
+        <AlertContentContainer>
+          <StyledH2>Warning</StyledH2>
+          <StyledSmall>
+            You are using a smart contract wallet. This is not recommended.{" "}
+            <StyledA
+              href="https://docs.kleros.io/kleros-faq#can-i-use-a-smart-contract-account-to-stake-in-the-court"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn more.
+            </StyledA>
+          </StyledSmall>
+        </AlertContentContainer>
+      </CustomAlert>
 
       <StyledIconButton
         small
